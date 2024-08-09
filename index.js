d9b5bf2dd3b0ffd712289caf346fe25a91db4618
rajeevprajapatt <rajeevprajapat06@gmail.com>
2024-08-09 23:28:56 +0530

    resume download route created

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const staticRouter = require("./routes/staticRouter");
 const mongoConnect = require("./connection");
+const session = require("express-session");
+
 
 const app = express();
 const PORT = 3001;
@@ -9,6 +11,13 @@ const PORT = 3001;
 mongoConnect("mongodb://127.0.0.1:27017/Portfolio").then(() => {
     console.log("Mongo Connect Successfully");
 })
+
+app.use(session({
+    secret: '$Rajeev007$',
+    resave: false,
+    saveUninitialized: true,
+}));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,18 +1,35 @@
 const express = require("express");
 const Message = require("../models/message");
+const nodemailer = require("nodemailer");
+const session = require("express-session");
+const path = require("path");
 
 const router = express.Router();
 
+// const transPorter = nodemailer.createTransport(
+//     {
+//         service: "gmail",
+//         auth: {
+//             user: "rajeevprajapat43@gmail.com",
+//             pass: "6378952948"
+//         }
+//     });
+
 router.get("/", (req, res) => {
-    return res.render('index');
+    const msg = req.session.message;
+    req.session.message = null;
+    return res.render('index',{
+        msg
+    });
 })
 
 router.post("/contactMessage", async (req, res) => {
     const body = req.body;
     try {
-        if (!body.name || !body.email || !body.message) return res.status(400).render("index", {
-            msg: "Please, fill all the fields"
-        });
+        if (!body.name || !body.email || !body.message){
+            req.session.message = "Please, fill all the fields";
+            return res.status(400).redirect("/");
+        } 
 
         await Message.create({
             name: body.name,
@@ -20,12 +37,33 @@ router.post("/contactMessage", async (req, res) => {
             message: body.message
         });
 
-        return res.render("index", {
-            msg: "Message sent successfully"
-        })
+        // const mailOption = {
+        //     from: "rajeevprajapat43@gmail.com",
+        //     to: body.email,
+        //     subject: "Sending Email using Node.js",
+        //     text: "this is the mail"
+        // }
+
+        // transPorter.sendMail(mailOption, (err, info) => {
+        //     if (err) {
+        //         console.log(err);
+        //     }
+        //     else {
+        //         console.log("Email sent: " + info.response);
+        //     }
+        // })
+        
+        req.session.message = "Message sent successfully";
+
+        return res.redirect("/");
     } catch (error) {
         console.log(error);
     }
+})
+
+router.get("/ResumeDownload",(req,res)=>{
+    const file = path.resolve("./public","files","RajeevPrajapatResume.pdf");
+    res.download(file);
 })
 
 module.exports = router;
